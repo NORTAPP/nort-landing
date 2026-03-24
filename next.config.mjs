5ed972253cb881b346e45ab19652146003aa8d73
Benson-Gitonga <166705606+Benson-Gitonga@users.noreply.github.com>
2026-03-24 21:21:53 +0300

    devops issue change

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // This tells the landing page to look for its files on the Vercel URL
+  // even when it's being viewed on nortapp.online
+  assetPrefix: 'https://nort-landing-nine.vercel.app',
+};
 
 export default nextConfig;
